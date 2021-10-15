3bc56b8be72457b25812931aa5a0a824283f8e5a
Paulo Jefferson Mendes Oliveira <dark_4862@hotmail.com>
2021-10-14 21:04:13 -0300

    Primeira classe finalizada.

--- ConsoleClassePessoa/Program.cs
+++ ConsoleClassePessoa/Program.cs
@@ -6,7 +6,38 @@ namespace ConsoleClassePessoa
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Paulo");
+            Pessoa pessoa = new Pessoa();
+
+            Console.WriteLine("Digite o nome: ");
+            pessoa.nome = Console.ReadLine();
+
+            Console.WriteLine("Digite sua idade: ");
+            pessoa.idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite seu RG: ");
+            pessoa.rg = Console.ReadLine();
+
+            Console.WriteLine("Digite seu CPF: ");
+            pessoa.cpf = Console.ReadLine();
+
+            Console.WriteLine("Nome: {0}", pessoa.nome);
+            Console.WriteLine("Idade: {0}", pessoa.idade);
+            Console.WriteLine("Rg: {0}", pessoa.rg);
+            Console.WriteLine("CPF: {0}", pessoa.cpf);
+
+            Pessoa pessoaSilvia = new Pessoa();
+            pessoaSilvia.nome = "Silvia Mendes";
+            pessoaSilvia.idade = 32;
+            pessoaSilvia.rg = "11.000.000-0";
+            pessoaSilvia.cpf = "111.000.000-00";
+
+            Console.WriteLine("Nome: {0}", pessoaSilvia.nome);
+            Console.WriteLine("Idade: {0}", pessoaSilvia.idade);
+            Console.WriteLine("Rg: {0}", pessoaSilvia.rg);
+            Console.WriteLine("CPF: {0}", pessoaSilvia.cpf);
+
+            pessoa.Acenar(pessoaSilvia);
+            pessoa.PerguntaIdade(pessoaSilvia);
         }
     }
 }
